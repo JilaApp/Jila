a5dfbbc5245df9d715444b895128fb2ff0bd1fb7
Jake Wang <jakewangzhi@gmail.com>
2024-10-15 15:41:59 -0500

    test

--- frontend/src/app/+not-found.tsx
+++ frontend/src/app/+not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <ThemedView className="flex-1 items-center justify-center p-5">
+      <ThemedView className="flex-1 items-center justify-center p-6">
         <ThemedText type="title">This screen doesn't exist.</ThemedText>
         <Link href="/" className="mt-4 py-4">
           <ThemedText type="link">Go to home screen!</ThemedText>
