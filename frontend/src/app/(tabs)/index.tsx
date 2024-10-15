f127355ac2f8dfc6b34162180cad57493567bcb1
Jake Wang <jakewangzhi@gmail.com>
2024-10-14 20:54:06 -0500

    testing

--- frontend/src/app/(tabs)/index.tsx
+++ frontend/src/app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcomee!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
