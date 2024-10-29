eb24300dc437248a323201722203c2eb03c9a1b1
Jake Wang <jakewangzhi@gmail.com>
2024-10-29 18:49:33 -0500

    change back

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         href="https://vercel.com/templates/next.js/postgres-prisma"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercell</p>
+        <p>Deploy your own to Vercel</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
