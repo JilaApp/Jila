d97bd4572a71f9a90973925aa0197f31bacbd7ca
Natasha Bhatia <tashabhatia27@gmail.com>
2024-11-05 18:57:03 -0600

    push current files

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,26 +1,63 @@
 import { Tabs } from "expo-router";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import { Colors } from "@/src/constants/Colors";
 import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const Tab = createBottomTabNavigator();
 
   return (
-    <Tabs
+    <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: true,
         headerTitleAlign: "center",
         headerTitle: () => <Text>Header</Text>,
+        tabBarStyle: {
+          backgroundColor: "#7E0601"
+        }   
       }}
-    >
+      >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: (tabInfo) => {
+            return (
+             <Image 
+                style={{height: 40, width: 40}} source={require('@/src/images/house.png')}
+              />
+            )
+          }
         }}
       />
-    </Tabs>
+      <Tabs.Screen
+        name="Resources"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: (tabInfo) => {
+            return (
+             <Image 
+                style={{height: 40, width: 40}} source={require('@/src/images/resources.png')}
+              />
+            )
+          }        
+        }}
+      />
+      <Tabs.Screen
+        name="Help"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: (tabInfo) => {
+            return (
+             <Image 
+                style={{height: 40, width: 40}} source={require('@/src/images/help.png')}
+              />
+            )
+          }        
+        }}
+      />
+    </Tab.Navigator>
   );
 }
