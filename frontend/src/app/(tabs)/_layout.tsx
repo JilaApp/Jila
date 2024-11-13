7b742e6dd8837433554aee48e72b8dba130a742b
John Speaks <113151039+JTSIV1@users.noreply.github.com>
2024-11-12 18:55:55 -0600

    sync w/ main (#23)
    * Bug: Fix recurring applying Prisma seed in prod db (#13)
    
    * test
    
    * testing
    
    * make sure only the seed file can be only applied in local
    
    * change back
    
    * created header component
    
    * fix cannot find png module issue
    
    * fix cannot find png module issue
    
    * removed header component usage, directly added header icon to tabs
    
    * latest header
    
    * removed hamburger icon
    
    * changed to tailwind css
    
    * push current files
    
    * finish implementing the footer
    
    * remove color theme
    
    * update styling
    
    * increase the header height
    
    * update home button
    
    * added main page content
    
    * changed all page backgrounds to gray
    
    * added basic layout of resources page + button (non functional for now, can remove if not needed)
    
    * Add footer (#18)
    
    * fix cannot find png module issue
    
    * push current files
    
    * finish implementing the footer
    
    * remove color theme
    
    * update styling
    
    * increase the header height
    
    * update home button
    
    * added main page content
    
    * changed all page backgrounds to gray
    
    * updates
    
    * update icons
    
    ---------
    
    Co-authored-by: Natasha Bhatia <tashabhatia27@gmail.com>
    Co-authored-by: alognz <ag131@illinois.edu>
    Co-authored-by: alondra <alondgonza@gmail.com>
    
    * Authentication helper (#17)
    
    * added admin table and updated videotype enum
    
    * seed changed
    
    * Update seed.ts for merge conflict
    
    ---------
    
    Co-authored-by: John Speaks <113151039+JTSIV1@users.noreply.github.com>
    
    * fixed recursos header layout & changed sound icon
    
    * fixed recursos header layout & changed sound icon
    
    * deleeted pngs
    
    ---------
    
    Co-authored-by: Jake-WangZhi <68337589+Jake-WangZhi@users.noreply.github.com>
    Co-authored-by: alognz <ag131@illinois.edu>
    Co-authored-by: Jake Wang <jakewangzhi@gmail.com>
    Co-authored-by: alondra <alondgonza@gmail.com>
    Co-authored-by: Natasha Bhatia <tashabhatia27@gmail.com>
    Co-authored-by: chloeadimonte <104934116+chloeadimonte@users.noreply.github.com>

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,24 +1,92 @@
 import { Tabs } from "expo-router";
-import { Text } from "react-native";
-import { Colors } from "@/src/constants/Colors";
-import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { Image, View } from "react-native";
+import { Entypo, FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: true,
         headerTitleAlign: "center",
-        headerTitle: () => <Text>Header</Text>,
+        tabBarStyle: {
+          backgroundColor: "#7E0601",
+          paddingTop: 32,
+          paddingBottom: 32,
+        },
+        headerStyle: {
+          backgroundColor: "#7E0601",
+          height: 120,
+        },
       }}
     >
       <Tabs.Screen
+        name="help/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <Ionicons
+                  name="help-circle-outline"
+                  size={40}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
+          headerTitle: () => (
+            <Ionicons name="help-circle-outline" size={64} color={"white"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center mt-0.5 ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <Entypo
+                  name="home"
+                  size={36}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
+          headerTitle: () => <Entypo name="home" size={56} color={"white"} />,
+        }}
+      />
+      <Tabs.Screen
+        name="resources/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <FontAwesome5
+                  name="book-open"
+                  size={27}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
+          headerTitle: () => (
+            <FontAwesome5 name="book-open" size={48} color={"white"} />
+          ),
         }}
       />
     </Tabs>
---
+++ frontend/src/app/(tabs)/resources/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, TouchableOpacity} from "react-native";
+import {AntDesign} from '@expo/vector-icons';
+
+export default function Resources() {
+
+  return (
+    <View className="flex-1 bg-gray-100 p-4">
+      
+      <View className="flex-row items-center justify-between mb-4">
+        <Text className="text-3xl font-bold text-[#7E0601]">Recursos</Text>
+        <TouchableOpacity className="w-8 h-8 rounded-full bg-[#7E0601] items-center justify-center">
+          <AntDesign name={"sound"} size={20} color={"white"} />
+        </TouchableOpacity>
+      </View>
+
+      <View className="bg-white rounded-2xl p-4 m-2">
+        <Text className="text-sm text-gray-800">This is the content</Text>
+      </View>
+
+      <TouchableOpacity className="flex-row w-1/2 self-center items-center justify-center py-2 px-4 bg-red-800 rounded-3xl mt-3">
+        <AntDesign name="plus" size={24} color="white" />
+        <Text className="text-white font-bold text-xl ml-1">More</Text>
+        </TouchableOpacity>
+
+      </View>
+  );
+}
