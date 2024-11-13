26e71f1495f03c8620db8ca0a26df7c11c0f4d1c
Jake-WangZhi <68337589+Jake-WangZhi@users.noreply.github.com>
2024-11-12 18:24:11 -0600

    Add footer (#18)
    * fix cannot find png module issue
    
    * push current files
    
    * finish implementing the footer
    
    * remove color theme
    
    * update styling
    
    * increase the header height
    
    * update home button
    
    * added main page content
    
    * changed all page backgrounds to gray
    
    * updates
    
    * update icons
    
    ---------
    
    Co-authored-by: Natasha Bhatia <tashabhatia27@gmail.com>
    Co-authored-by: alognz <ag131@illinois.edu>
    Co-authored-by: alondra <alondgonza@gmail.com>

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,17 +1,18 @@
 import { Tabs } from "expo-router";
-import { useColorScheme } from "@/src/hooks/useColorScheme";
-import { Image } from "react-native";
-import home from "@/assets/images/home.png";
+import { Image, View } from "react-native";
+import { Entypo, FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#7E0601",
         headerShown: true,
         headerTitleAlign: "center",
+        tabBarStyle: {
+          backgroundColor: "#7E0601",
+          paddingTop: 32,
+          paddingBottom: 32,
+        },
         headerStyle: {
           backgroundColor: "#7E0601",
           height: 120,
@@ -19,11 +20,72 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
+        name="help/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <Ionicons
+                  name="help-circle-outline"
+                  size={40}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
+          headerTitle: () => (
+            <Ionicons name="help-circle-outline" size={64} color={"white"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center mt-0.5 ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <Entypo
+                  name="home"
+                  size={36}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
+          headerTitle: () => <Entypo name="home" size={56} color={"white"} />,
+        }}
+      />
+      <Tabs.Screen
+        name="resources/index"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View
+                className={`w-12 h-12 rounded-full justify-center items-center ${
+                  focused ? "bg-white" : "bg-transparent"
+                }`}
+              >
+                <FontAwesome5
+                  name="book-open"
+                  size={27}
+                  color={focused ? "#7E0601" : "black"}
+                />
+              </View>
+            );
+          },
           headerTitle: () => (
-            <Image source={home} className="w-12 h-12" />
+            <FontAwesome5 name="book-open" size={48} color={"white"} />
           ),
         }}
       />
