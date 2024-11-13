cea5b5471932bab307f7efcdec2106ef418acb0d
alognz <ag131@illinois.edu>
2024-11-12 18:37:55 -0600

    fixed recursos header layout & changed sound icon

--- frontend/src/app/(tabs)/resources/index.tsx
+++ frontend/src/app/(tabs)/resources/index.tsx
@@ -2,34 +2,24 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { styled } from "nativewind";
-import soundIcon from "@/assets/images/soundicon.png";
 
-const IconButton = styled(TouchableOpacity);
 export default function Resources() {
-
-  const handleSoundIconPress = () => {
-    // for once we have audio
-  };
-
-  const handleMorePress = () => {
-
-  };
 
   return (
     <View className="flex-1 bg-gray-100 p-4">
-
-      <View className="flex-row items-center justify-center mb-4">
-        <TouchableOpacity onPress={handleSoundIconPress} className="mr-2">
-          <Image source={soundIcon} className="w-7 h-7" />
+      
+      <View className="flex-row items-center justify-between mb-4">
+        <Text className="text-3xl font-bold text-[#7E0601]">Recursos</Text>
+        <TouchableOpacity className="w-8 h-8 rounded-full bg-[#7E0601] items-center justify-center">
+          <AntDesign name={"sound"} size={20} color={"white"} />
         </TouchableOpacity>
-        <Text className="text-3xl font-bold text-red-900">Recursos</Text>
       </View>
 
-      <View className="bg-white rounded-3xl p-4 m-2">
+      <View className="bg-white rounded-2xl p-4 m-2">
         <Text className="text-sm text-gray-800">This is the content</Text>
       </View>
 
-      <TouchableOpacity onPress={handleMorePress} className="flex-row w-1/2 self-center items-center justify-center py-2 px-4 bg-red-800 rounded-3xl mt-3">
+      <TouchableOpacity className="flex-row w-1/2 self-center items-center justify-center py-2 px-4 bg-red-800 rounded-3xl mt-3">
         <AntDesign name="plus" size={24} color="white" />
         <Text className="text-white font-bold text-xl ml-1">More</Text>
         </TouchableOpacity>
