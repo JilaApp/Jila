f3e9944ec1d42cc709635fe5cb8142267b5a4f84
Jake Wang <jakewangzhi@gmail.com>
2024-11-12 18:10:53 -0600

    updates

--- frontend/src/app/(tabs)/index.tsx
+++ frontend/src/app/(tabs)/index.tsx
@@ -1,55 +1,60 @@
-import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
-import { styled } from "nativewind";
-import soundIcon from "@/assets/images/soundicon.png";
-
-const IconButton = styled(TouchableOpacity);
+import { View, Text, TouchableOpacity } from "react-native";
+import { FontAwesome, AntDesign, Entypo } from "@expo/vector-icons";
 
 export default function HomeScreen() {
   const topics = [
-    { name: "Legal", icon: "balance-scale", color: "#F3722C", enabled: false },
-    { name: "Transport", icon: "bus", color: "#577590", enabled: false },
-    { name: "Medical", icon: "heartbeat", color: "#F9C74F", enabled: false },
-    { name: "Professional Development", icon: "suitcase", color: "#90BE6D", enabled: false },
-    { name: "General", icon: "bell", color: "#682388", enabled: false },
+    { name: "Transport", icon: "bus", color: "#577590", enabled: true },
+    { name: "Legal", icon: "balance-scale", color: "#9F2020", enabled: true },
+    { name: "Medical", icon: "heartbeat", color: "#F9C74F", enabled: true },
+    {
+      name: "Career",
+      icon: "suitcase",
+      color: "#90BE6D",
+      enabled: true,
+    },
+    {
+      name: "Other",
+      icon: "dots-three-horizontal",
+      color: "#9C9C9C",
+      enabled: true,
+      type: "Entypo",
+    },
   ];
 
-  const handleSoundIconPress = () => {
-    // for once we have audio
-  };
-
   return (
-    <View className="flex-1 bg-gray-100 p-4">
-      
-      <View className="flex-row items-center justify-center mb-4">
-        <TouchableOpacity onPress={handleSoundIconPress} className="mr-2">
-          <Image source={soundIcon} className="w-7 h-7" />
+    <View className="flex-1 bg-[#E4E4E4] p-4">
+      <View className="flex-row items-center justify-between mb-4">
+        <Text className="text-3xl font-bold text-[#7E0601]">All Topics</Text>
+        <TouchableOpacity className="w-8 h-8 rounded-full bg-[#7E0601] items-center justify-center">
+          <AntDesign name={"sound"} size={20} color={"white"} />
         </TouchableOpacity>
-        <Text className="text-3xl font-bold text-red-900">All Topics</Text>
       </View>
-      
-      <View className="flex-wrap flex-row justify-between">
+
+      <View className="flex-wrap flex-row justify-between bg-[#D9D9D9] p-6 rounded">
         {topics.map((topic, index) => (
-          <View key={index} className="items-center m-2 w-24">
-            <IconButton
-              className="w-24 h-24 items-center justify-center rounded-lg"
+          <View key={index} className="items-center m-2 w-16">
+            <TouchableOpacity
+              className={`w-16 h-16 items-center justify-center rounded-lg`}
               style={{
-                backgroundColor: topic.enabled ? topic.color : "#D3D3D3", 
-              }}
-              onPress={() => {
-                if (topic.enabled) {
-                }
+                backgroundColor: topic.enabled ? topic.color : "#D3D3D3",
               }}
               disabled={!topic.enabled}
             >
-              <FontAwesome
-                name={topic.icon as any}
-                size={50}
-                color={topic.enabled ? "white" : "gray"}
-              />
-            </IconButton>
-            <Text className="text-center font-bold mt-2 text-gray-700">
+              {topic.type === "Entypo" ? (
+                <Entypo
+                  name={topic.icon as keyof typeof Entypo.glyphMap}
+                  size={37}
+                  color={topic.enabled ? "white" : "gray"}
+                />
+              ) : (
+                <FontAwesome
+                  name={topic.icon as keyof typeof FontAwesome.glyphMap}
+                  size={37}
+                  color={topic.enabled ? "white" : "gray"}
+                />
+              )}
+            </TouchableOpacity>
+            <Text className="text-center font-bold mt-2 text-black">
               {topic.name}
             </Text>
           </View>
