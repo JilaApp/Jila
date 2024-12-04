ce1d5e7714c94127bbb7d009fdc69cce69234c96
Natasha Bhatia <tashabhatia27@gmail.com>
2024-12-03 19:02:07 -0600

    Changes to video editor

--- frontend/src/app/(tabs)/(home)/details/[category]/videos/index.tsx
+++ frontend/src/app/(tabs)/(home)/details/[category]/videos/index.tsx
@@ -1,22 +1,30 @@
 import { useRouter } from "expo-router";
-import { Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { Text, View, Button } from "react-native";
+import React, { useState, useCallback } from "react";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function videos() {
   const router = useRouter();
 
-  return (
-    <View className="flex-1 bg-gray-100 justify-center items-center">
-      <Text className="text-black">Hello, this is the new page</Text>
-      <TouchableOpacity
-        onPress={() => {
-          router.back();
-        }}
-      >
-        <View className="h-12 w-12 bg-blue-400 flex items-center justify-center">
-          <Text className="text-white">Back</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+  const [playing, setPlaying] = useState(false);
+  const togglePlaying = () => {
+    setPlaying((prev) => !prev);
+    return (
+      <View className="flex-1 bg-gray-100 justify-center items-center">
+        <Text className="text-black">Hello, this the new page</Text>
+        <TouchableOpacity
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <YoutubePlayer height={300} play={playing} videoId={"pznY3NUZfFg"} />
+          <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+          <View className="h-12 w-12 bg-blue-400 flex items-center justify-center">
+            <Text className="text-white">Back</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 }
